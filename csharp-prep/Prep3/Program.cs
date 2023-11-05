24c3453d25da100f07f0524b2629d79c0e322c34
stanleynephi <nul23001@byui.edu>
2023-11-05 10:53:52 +0000

    completed the prep assignment for week 1 of class, worked with list, input and outputs and loops as well.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -7,34 +7,34 @@ class Program
         Console.WriteLine("Hello Prep3 World!");
 
         Random randomnumber = new Random();
-int number = randomnumber.Next(1,11);
+        int number = randomnumber.Next(1,11);
 
-int guess = -1; 
-string again = "yes";
-//let the user guess the number
-while (guess != number && again == "yes") 
-{
-    Console.WriteLine("What is the magic number? ");
-    guess = int.Parse(Console.ReadLine());
+        int guess = -1; 
+        string again = "yes";
+        //let the user guess the number
+        while (guess != number && again == "yes") 
+        {
+            Console.WriteLine("What is the magic number? ");
+            guess = int.Parse(Console.ReadLine());
 
-    if (guess > number)
-    {
-        Console.WriteLine("Too high! Try Again.");
-    }
+            if (guess > number)
+            {
+                Console.WriteLine("Too high! Try Again.");
+            }
 
-    else if (guess < number)
-    {
-        Console.WriteLine("Too low! Try Again.");
-    }
+            else if (guess < number)
+            {
+                Console.WriteLine("Too low! Try Again.");
+            }
 
-    else
-    {
-        Console.WriteLine("That was a correct guess well done");
-    }
+            else
+            {
+                Console.WriteLine("That was a correct guess well done");
+            }
 
 
-    Console.WriteLine("Will you play again?");
-    again = Console.ReadLine();
-}
+            Console.WriteLine("Will you play again?");
+            again = Console.ReadLine();
+        }
     }
 }
