addbe30b452fb745cb156e159b741d148ed10d24
stanleynephi <nul23001@byui.edu>
2023-11-05 15:50:23 +0000

    completed the prep assignment for week 1 of class, worked on functions.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -10,42 +10,42 @@ class Program
         Console.WriteLine("Hello Prep4 World!");
 
 
-//create a new list in C#
-List<int> numbers1 = new List<int>();
+        //create a new list in C#
+        List<int> numbers1 = new List<int>();
 
-int num = -1;
-int result = 0;
+        int num = -1;
+        int result = 0;
 
 
-while (num != 0)
-{
-    Console.WriteLine("Please enter a number  ");
-    num = int.Parse(Console.ReadLine());
-    if (num != 0)
-    {
-        numbers1.Add(num);
-    }
-
-    if (num == 0)
-    {
-        Console.WriteLine("The numbers you entered are: ");
-
-        int maxnumber = numbers1.Max();
-        foreach (int number in numbers1)
+        while (num != 0)
         {
-            Console.WriteLine(number);
-            result += number;
-        }
+            Console.WriteLine("Please enter a number  ");
+            num = int.Parse(Console.ReadLine());
+            if (num != 0)
+            {
+                numbers1.Add(num);
+            }
 
-        Console.WriteLine($"Your total was {result}");
-        Console.WriteLine($"The Max number is {maxnumber}");
-        Console.WriteLine($"The Total Number in the list is {numbers1.Count()}");
+            if (num == 0)
+            {
+                Console.WriteLine("The numbers you entered are: ");
 
-        float average = ((float) result)/numbers1.Count();
-        Console.WriteLine($"Average of all numbers is {average}");
+                int maxnumber = numbers1.Max();
+                foreach (int number in numbers1)
+                {
+                    Console.WriteLine(number);
+                    result += number;
+                }
 
-    }
-}  
+                Console.WriteLine($"Your total was {result}");
+                Console.WriteLine($"The Max number is {maxnumber}");
+                Console.WriteLine($"The Total Number in the list is {numbers1.Count()}");
+
+                float average = ((float) result)/numbers1.Count();
+                Console.WriteLine($"Average of all numbers is {average}");
+
+            }
+        }  
     }
     
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,59 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+
+
+        WelcomeMessage();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, squaredNumber);
+
+
+        // FUNCTION TO DISPLAY A WELCOME MESSAGE        
+
+        static string WelcomeMessage()
+        {
+            Console.WriteLine("Welcome Home");
+            return "Welcome Home";
+        }
+
+
+        // FUNCTION TO ASK THE USER FOR HIS NAME
+        static string PromptUserName()
+        {
+            Console.WriteLine("What is your name? ");
+            string name = Console.ReadLine();
+
+    Console.WriteLine($"{name}");
+            return name;
+
+        }
+
+
+//FUNCTION TO ASK USER FOR HIS FAVOURITE NUMBER
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("Whats is your favourite number? ");
+            int num = int.Parse(Console.ReadLine());
+            return num;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int result = number * number;
+            return result;
+        }
+
+
+        static void DisplayResult(string name, int result)
+        {
+            Console.WriteLine($"{name} the square of your favourite number is {result}");
+        }
+
+
     }
 }
